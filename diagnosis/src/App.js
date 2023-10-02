8e2feef6f9e369bb3f003c4cdeb0f90628abd027
Marco <mrovell@ucla.edu>
2023-10-02 15:50:48 -0700

    Limited functionality, runs on ports 3000 and 3001

--- diagnosis/src/App.js
+++ diagnosis/src/App.js
@@ -5,60 +5,96 @@ const App = () => {
   const [input2, setInput2] = useState('');
   const [input3, setInput3] = useState('');
 
-  const handleButtonClick = async (input) => {
+  const saveFile = async () => {
+    console.log('worked!');
+    // const gender = document.getElementById("selGender");
+    // const age = document.getElementById("numAge");
+    // const Symptom1 = input1;
+    // const Symptom2 = input2;
+    // const Symptom3 = input3;
+    // const text = document.getElementById("msg");
+
     try {
-      const response = await fetch('/run-python-script', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          input
-        })
+      const response = await fetch('http://localhost:3001/run-python-script', { // need to be able to run on any port that server opens on
+        method: 'POST' 
       });
 
       if (response.ok) {
-        const result = await response.text();
-        console.log('Python script execution result:', result);
+        console.log('Python script executed successfully!');
       } else {
-        console.error('Failed to execute Python script:', response.statusText);
+        console.error('Failed to execute Python script.');
       }
     } catch (error) {
       console.error('Error:', error);
     }
-  };
+    // let data = "\r"
+  }
+
+  // const modelPath = '../model.py';
+  // exec('python ${modelPath}', (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error('Error executing model script: ${error.message}');
+  //     return;
+  //   }
+  //   console.log('model script output:', stdout);
+  // });
+
+
+  // const handleButtonClick = async (input) => {
+  //   try {
+  //     const response = await fetch('/run-python-script', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({
+  //         input
+  //       })
+  //     });
+
+  //     if (response.ok) {
+  //       const result = await response.text();
+  //       console.log('Python script execution result:', result);
+  //     } else {
+  //       console.error('Failed to execute Python script:', response.statusText);
+  //     }
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //   }
+  // };
 
   return (
     <div>
       <input
-        type="text"
-        placeholder="Input for Symptom 1"
-        value={input1}
-        onChange={(e) => setInput1(e.target.value)}
-      />
-      <button>Symptom 1</button>
-
+        type="text" placeholder="Symptom 1" value={input1}
+        onChange={(e) => setInput1(e.target.value)} />
       <input
-        type="text"
-        placeholder="Input for Symptom 2"
-        value={input2}
-        onChange={(e) => setInput2(e.target.value)}
-      />
-      <button>Symptom 2</button>
-
+        type="text" placeholder="Symptom 2" value={input2}
+        onChange={(e) => setInput2(e.target.value)} />
       <input
-        type="text"
-        placeholder="Input for Symptom 3"
-        value={input3}
-        onChange={(e) => setInput3(e.target.value)}
-      />
-      <button>Symptom 3</button>
+        type="text" placeholder="Symptom 3" value={input3}
+        onChange={(e) => setInput3(e.target.value)} />
+      <input type="number" id="numAge" placeholder="Enter your age" />
+      <select id = "selGender">
+        <option selected value="">-- Choose Gender --</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+        <option value="Other">Other</option>
+        <option value="N/A">Prefer Not To Disclose</option>
+      </select>
 
+      <div>
+        <textarea id="msg" 
+          name="msg" 
+          maxLength={250} 
+          style={{ height: '200px', width: '500px'}} 
+          placeholder="Give us some more detail about your symtoms... 250 characters max!">
+        </textarea>
+      </div>
       <body>
-        <button>Submit</button>
+        <input type="button" id="bt" value="Input Save Data" onClick={saveFile} />
       </body>
     </div>
-
 
   );
 };
--- diagnosis/src/server.js
+++ diagnosis/src/server.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const { exec } = require('child_process');
+import express from 'express';
+import { exec } from 'child_process';
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(express.json());
 
 app.post('/run-python-script', (req, res) => {
-  exec('python ../model.py', (error, stdout, stderr) => {
+  const question = "Give me a diagnosis based on the current symptoms I am exhibiting?";
+  exec('python /GPT-Query-App/diagnosis/model.py ' + question, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error executing Python script: ${error.message}`);
-      return res.status(500).send('Internal server error.');
+      console.error(`Error executing model script: ${error.message}`);
+      res.status(500).send('Internal server error.');
+      return;
     }
-    console.log(`Python script output: ${stdout}`);
-    res.send('Python script executed successfully.');
+    console.log(`model script output: ${stdout}`);
+    res.send('model script executed successfully.');
   });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default port;
