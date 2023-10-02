2fc8dacc74b886f96a3662e944992d3817c0dfe0
Marco <mrovell@ucla.edu>
2023-10-02 15:50:48 -0700

    Limited functionality, runs on ports 3000 and 3001

--- diagnosis/src/server.js
+++ diagnosis/src/server.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const { exec } = require('child_process');
+import express from 'express';
+import { exec } from 'child_process';
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT;
+const port = 3001;
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.post('/run-python-script', (req, res) => {
-  exec('python ../model.py', (error, stdout, stderr) => {
+  const question = 'give a numbered list of possible illnesses or health-issues I may be experiencing according to the information provided';
+  exec('python model.py ' + question, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error executing Python script: ${error.message}`);
-      return res.status(500).send('Internal server error.');
+      console.error(`Error executing model script: ${error.message}`);
+      res.status(500).send('Internal server error.');
+      return;
     }
-    console.log(`Python script output: ${stdout}`);
-    res.send('Python script executed successfully.');
+    // console.log(`${stdout}`); 
+    res.send({stdout: stdout});
   });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default port;
